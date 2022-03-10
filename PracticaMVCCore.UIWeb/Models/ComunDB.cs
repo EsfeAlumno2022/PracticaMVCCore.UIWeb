dcee851cfa347b5b3157bef4480e468555c5329a
Marvin Antonio Barrera Trigueros <antoniobarreratrigueros@hotmail.com>
2022-03-10 17:13:14 -0600

    Practica 2022-03-10 17:13

---
+++ PracticaMVCCore.UIWeb/Models/ComunDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// ********************************
+using Microsoft.EntityFrameworkCore;
+namespace PracticaMVCCore.UIWeb.Models
+{
+    public class ComunDB: DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DEV01\SQLEXPRESS02;Initial Catalog=SeguridadWebdb;Integrated Security=True");
+        }
+    }
+}
---
+++ PracticaMVCCore.UIWeb/Models/Rol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// ********************************
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PracticaMVCCore.UIWeb.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }      
+    }
+}
